52c827d9fb0c14cb4e7187b94123f5a312482897
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-09-08 18:56:26 +0200

    fixed bug

--- lua/auto-brackets/init.lua
+++ lua/auto-brackets/init.lua
@@ -1,6 +1,7 @@
 
 local M = {}
 
+
 function M.brackets()
 	print("Hello ()")
 
@@ -11,7 +12,7 @@ function M.brackets()
 
 	local pos = vim.api.nvim_win_get_cursor(0)[2]
 	local line = vim.api.nvim_get_current_line()
-	local nline = line:sub(0, pos - 1) .. '(' .. line:sub(pos - 1, pos) .. ')' .. line:sub(pos + 1)
+	local nline = line:sub(0, pos) .. '()' .. line:sub(pos + 1)
 	vim.api.nvim_set_current_line(nline)
 end
 
