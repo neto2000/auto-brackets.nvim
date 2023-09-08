fbd5b832e2db9e6496f4ddddebebc78a59d11204
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-09-08 18:42:58 +0200

    write brackets

--- lua/auto-brackets/init.lua
+++ lua/auto-brackets/init.lua
@@ -3,6 +3,10 @@ local M = {}
 
 function M.brackets()
 	print("Hello ()")
+
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+	vim.api.nvim_buf_set_text(0, row, col, row, col + 2, '()')
 end
 
 
